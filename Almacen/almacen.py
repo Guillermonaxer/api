e104223225d41d25b2b9e61805e8424ea591d1b1
Juan <79379448+JuanContrerasRobles@users.noreply.github.com>
2022-07-14 11:23:02 +0200

    Update almacen y añado api_doc.yaml

--- Almacen/almacen.py
+++ Almacen/almacen.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify,request
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask import Flask, jsonify,request, send_from_directory
 import argparse
 import yaml
 import sqlite3
@@ -12,17 +13,20 @@ def create_app():
     return app
 app = create_app()
 
+
 # Se lee el archivo config.yaml y se le asigna un identificador al consumidor de la api
 
 with open('config.yaml', 'r') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-    config['basedatos']['consumidor_almacen_api']=str(uuid.uuid1())
+    config['basedatos']['consumidor_almacen_api'] = str(uuid.uuid1())
     print(config)
+
 
 # Se escriben las modificaciones anteriores con el id añadido
 
 with open('config.yaml', "w") as payload:
     payload.write(yaml.dump(config))
+
 
 # Se especifican argumentos
 
@@ -31,6 +35,7 @@ parser.add_argument("--servidor", metavar="string", default="localhost", require
 parser.add_argument("--puerto", type=int, default=5000, required=False, help="Especifica el puerto del servidor")
 parser.add_argument("--config", metavar="string", required=True, help="Requiere un archivo de configuracion .yaml") 
 args = parser.parse_args()
+
 
 # Se crea la base de datos si no existe
 
@@ -48,124 +53,173 @@ else:
 
 def solicitar_permisos(key):
     with open('config.yaml', 'r') as f:
-     config = yaml.load(f, Loader=yaml.FullLoader)
-     key_config=config['basedatos']['consumidor_almacen_api']
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        key_config = config['basedatos']['consumidor_almacen_api']
     print(config)
-    if key!= key_config:
-        validacion=False
+    if key != key_config:
+        validacion = False
     else: 
-        validacion=True
+        validacion = True
     return validacion
     
     
-# Se crea la tabla de la base de datos  
+# Se crea la ruta y la funcion para crear la tabla en la base de datos  
 
-@app.route('/api/almacen/crear-tabla', methods=['POST'])
+@app.route('/api/almacen/tabla', methods=['POST'])
 def crear_tabla():
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-     cur= con.cursor()
-     cur.execute('''CREATE TABLE if not exists producto('name','id','description','precio','unidades','stock')''')
-     con.commit()
-     return "Tabla creada"
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE if not exists producto('name','id','description','precio','unidades','stock')''')
+        con.commit()
+        return "Tabla creada"
     else:
-        error='No autorizado'
-        return jsonify(error)  
+        error = 'No autorizado'
+        return jsonify(error)
+
+
+# Se crea la ruta y la funcion para eliminar la tabla de la base de datos
+
+@app.route('/api/almacen/tabla', methods=['DELETE'])
+def eliminar_tabla():
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        cur.execute("DROP TABLE producto")
+        return "Tabla eliminada"
+    else:
+        error = 'No autorizado'
+        return jsonify(error)
+
 
 # Se crea el producto en la tabla
 
-@app.route('/api/almacen/crear-producto', methods=['POST'])
+@app.route('/api/almacen/producto', methods=['POST'])
 def crear_producto():
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-     cur=con.cursor()
-     cur.execute("INSERT INTO producto VALUES ('Barra de pan', 1, 'Barra de pan artesana', '1.50€', 3, True)")
-     con.commit()
-     return "Producto creado"
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        cur.execute("INSERT INTO producto VALUES ('Barra de pan', 1, 'Barra de pan artesana', '1.50€', 3, True)")
+        con.commit()
+        return "Producto creado"
     else:
-        error='No autorizado'
-        return jsonify(error)   
+        error = 'No autorizado'
+        return jsonify(error)  
+ 
 
 # Se crea la ruta para incrementar las unidades del producto
 
 @app.route('/api/almacen/incrementar-producto', methods=['PUT'])
 def incrementar_producto():
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-     cur=con.cursor()
-     cur.execute("UPDATE producto SET unidades = unidades + 1")
-     con.commit()
-     sentencia = "SELECT * FROM producto;"
-     cur.execute(sentencia)
-     titulo = "El producto se ha incrementado en una unidad"
-     stock = cur.fetchall()
-     return jsonify(titulo, stock)
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        cur.execute("UPDATE producto SET unidades = unidades + 1")
+        con.commit()
+        sentencia = "SELECT * FROM producto;"
+        cur.execute(sentencia)
+        titulo = "El producto se ha incrementado en una unidad"
+        stock = cur.fetchall()
+        return jsonify(titulo, stock)
     else:
         error='No autorizado'
         return jsonify(error)   
 
-# Se crea la ruta para decrementar las unidades del producto
+
+# Se crea la ruta y la funcion para decrementar las unidades del producto
 
 @app.route('/api/almacen/decrementar-producto', methods=['PUT'])
 def decrementar_producto():
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-     cur=con.cursor()
-     cur.execute("UPDATE producto SET unidades = unidades - 1")
-     con.commit()
-     sentencia = "SELECT * FROM producto;"
-     cur.execute(sentencia)
-     titulo = "El producto se ha decrementado en una unidad"
-     stock = cur.fetchall()
-     return jsonify(titulo, stock)
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        cur.execute("UPDATE producto SET unidades = unidades - 1")
+        con.commit()
+        sentencia = "SELECT * FROM producto;"
+        cur.execute(sentencia)
+        titulo = "El producto se ha decrementado en una unidad"
+        stock = cur.fetchall()
+        return jsonify(titulo, stock)
     else:
-        error='No autorizado'
+        error = 'No autorizado'
         return jsonify(error)  
-# Se crea la ruta para eliminar el producto
+
+
+# Se crea la ruta y la funcion para eliminar el producto de la tabla
     
-@app.route('/api/almacen/eliminar-producto', methods=['DELETE'])
+@app.route('/api/almacen/producto', methods=['DELETE'])
 def eliminar_producto():
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-     cur=con.cursor()
-     cur.execute("DROP TABLE producto")
-     con.commit
-     return "Producto eliminado"
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        cur.execute("DELETE FROM producto")
+        con.commit
+        return "Producto eliminado"
     else:
-        error='No autorizado'
+        error = 'No autorizado'
         return jsonify(error)  
 
-# Se crea la ruta para saber el stock del producto
 
-@app.route('/api/almacen/leer-producto', methods=['GET'])
+# Se crea la ruta y la funcion para saber el stock del producto
+
+@app.route('/api/almacen/producto', methods=['GET'])
 def leer_producto():
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-      cur = con.cursor()
-      sentencia = "SELECT * FROM producto;"
-      cur.execute(sentencia)
-      stock = cur.fetchall()
-    
-      return jsonify(stock)
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        sentencia = "SELECT * FROM producto;"
+        cur.execute(sentencia)
+        stock = cur.fetchall()
+        return jsonify(stock)
     else:
-        error='No autorizado'
+        error = 'No autorizado'
         return jsonify(error)  
     
-#leemos un id determinado de un producto
 
-@app.route('/api/almacen/leer-producto/<id>', methods=['GET'])
+# Se crea la ruta y la funcion para leer un id determinado de un producto
+
+@app.route('/api/almacen/producto/<id>', methods=['GET'])
 def leer_producto_determinado(id):
-    key=request.headers.get('key')
-    if solicitar_permisos(key) ==True:
-      cur = con.cursor()
-      sentencia = "SELECT*FROM producto where id="+id
-      cur.execute(sentencia)
-      stock = cur.fetchone()
-    
-      return jsonify(stock)
+    key = request.headers.get('key')
+    if solicitar_permisos(key) == True:
+        cur = con.cursor()
+        sentencia = "SELECT*FROM producto where id="+id
+        cur.execute(sentencia)
+        stock = cur.fetchone()
+        return jsonify(stock)
     else:
-        error='No autorizado'
+        error = 'No autorizado'
         return jsonify(error) 
+
+
+# Se crea la ruta y la funcion para acceder a la documentacion de la API
+
+@app.route('/services/spec')
+def get_spec():
+    return send_from_directory(app.root_path, "../Documentacion/api_doc.yaml")
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/services/spec' 
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
+
 # Se ejecuta la aplicacion
 
 if __name__ == '__main__':
